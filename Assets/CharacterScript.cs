6f206d159b6936c12aad2c769bbbcb0dd4011bde
AlexOrtega301 <estudiante01913.cfia@labmexia.gob.mx>
2026-05-16 11:45:45 -0600

    Add final version of runner

---
+++ Assets/CharacterScript.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{    
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float speed;
+    [SerializeField] float shift;  
+    [SerializeField] float jumpForce;  
+    [SerializeField] Animator anim; 
+    [SerializeField] TMP_Text score;  
+    [SerializeField] GameObject menu;
+    [SerializeField] GameObject shield;
+    [SerializeField] GameObject itemVFX, shieldVFX, obstacleVFX;
+    [SerializeField] AudioClip itemSFX, shieldSFX, obstacleSFX, destroySFX;
+    [SerializeField] AudioSource sound, music;  
+    float roundScore;
+    bool isGameOver;   
+    bool isShield;
+    void Start()
+    {         
+    }     
+    void Update() 
+    {       
+        if(!isGameOver)
+        {    
+            roundScore += Time.deltaTime;
+            score.text = "Score: " + roundScore.ToString("f1");        
+        
+            if(Input.GetKeyDown(KeyCode.A) && transform.position.x != -shift)
+            {
+                transform.Translate(-shift, 0, 0);
+            }
+            if(Input.GetKeyDown(KeyCode.D) && transform.position.x != shift)
+            {
+                transform.Translate(shift, 0, 0);
+            }
+
+            if(Input.GetKeyDown(KeyCode.Space) && rb.linearVelocity.y == 0)
+            {
+                rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);  
+                                
+            }
+            if(rb.linearVelocity.y > 0)   
+            {
+                anim.SetBool("jump", true);  
+            }
+            if(rb.linearVelocity.y == 0)
+            {
+                anim.SetBool("jump", false); 
+            } 
+        }       
+    }
+    void FixedUpdate()
+    {
+        if(!isGameOver)
+        {
+            rb.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);       
+        }
+    }   
+    void DeactivateShield()
+    {
+        isShield = false;        
+    }
+    
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.CompareTag("Obstacle"))
+        {
+            if(isShield)
+            {
+                Destroy(other.gameObject);
+                sound.clip = destroySFX;
+                sound.Play();
+            }
+            else
+            {
+                isGameOver = true;            
+                menu.SetActive(true);
+                anim.SetBool("death", true);
+
+                GameObject vfx = Instantiate(obstacleVFX, transform.position, transform.rotation);
+                Destroy(vfx, 3f); 
+
+                sound.clip = obstacleSFX;
+                sound.Play();
+                music.Stop();                
+            }            
+        }        
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("Money"))
+        {
+            roundScore += 5;
+            score.text = "Score: " + roundScore.ToString("f1"); 
+
+            GameObject vfx = Instantiate(itemVFX, other.transform.position, other.transform.rotation);
+            Destroy(vfx, 3f); 
+
+            sound.clip = itemSFX;
+            sound.Play(); 
+            Destroy(other.gameObject);
+        }
+        if(other.CompareTag("Shield"))
+        {
+            isShield = true;
+            Invoke("DeactivateShield", 5f);
+
+            GameObject vfx = Instantiate(shieldVFX, transform.position + transform.up, other.transform.rotation);
+            vfx.transform.SetParent(this.transform);
+            Destroy(vfx, 5f);
+            
+            sound.clip = shieldSFX;            
+            sound.Play();               
+            Destroy(other.gameObject);
+        }
+    }
+}
